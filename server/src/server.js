67d8a87737abfbc9894c55dd1e1b825df93ec5c7
William01br <williamstcorreia123@gmail.com>
2026-01-23 09:25:04 -0300

    chore: edit Dockerfile and move package.json filesto server directory

---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+import http from "node:http";
+
+const SERVER_HOST = process.env.SERVER_HOST;
+const SERVER_PORT = process.env.SERVER_PORT;
+
+async function main() {
+  try {
+    const server = http.createServer();
+    server.on("request", async (req, res) => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(`Hello from ${SERVER_HOST} `));
+    });
+    server.listen(SERVER_PORT);
+  } catch (err) {
+    console.error("Something went wrong...", err);
+  }
+}
+
+main()
+  .then(() => console.log(`Server started on ${SERVER_HOST}:${SERVER_PORT}!`))
+  .catch((err) => console.error("Something went wrong...", err));
